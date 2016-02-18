7c8d46090b6f3e21d74eb7f067a68070966cb76d
Kevin Dang <danggkev@gmail.com>
2016-02-18 20:16:20 +0000

    analytics lab

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,19 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res){
+	var random_num = Math.random();
+	console.log(random_num);
+
+	if(random_num >= 0) {
+		projects["grid"] = false;
+	  	res.render('index', projects);
+  	}
+  	else {
+  		res.redirect('/grid');
+  	}
+};
+
+exports.viewGrid = function(req, res){
+	projects["grid"] = true;
   	res.render('index', projects);
 };
